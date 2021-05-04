9e34d5fd3915c87da930230f1c0fff33da423474
Bomsiwakorn <bomsiwakorns@gmail.com>
2021-05-04 22:56:12 +0700

    Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    users: []
   },
   mutations: {
+    setUsers(state, value) {
+      state.users = value
+    },
+    clearUsers(state) {
+      state.users = []
+    },
   },
   actions: {
+    setUsers({commit}, value) {
+      commit('setUsers', value)
+    },
+    clearUsers({commit}) {
+      commit('clearUsers')
+    },
   },
   modules: {
+  },
+  getters: {
+    getUsers(state) {
+      return state.users
+    }
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Home Page</h2>
+    <button @click.prevent="setUsers()">Set Users</button>
+    <button style="margin-left: 1rem;" @click.prevent="clearUsers()">Clear Users</button>
+
+    <!-- Loop users data from store -->
+    <h3>Users in store</h3>
+    <ul>
+      <li v-for="(user,index) in $store.getters.getUsers" :key="index">{{user.firstName}} {{user.lastName}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      users: [
+        // Mockup Data
+        {firstName: 'Siwakorn', lastName: 'Jindajamorn'},
+        {firstName: 'John', lastName: 'Doe'},
+        {firstName: 'Lisa', lastName: 'Backpink'},
+      ]
+    }
+  },
+  methods: {
+    setUsers() {
+      // Set users data in store
+      this.$store.dispatch('setUsers', this.users)
+    },
+    clearUsers() {
+      // Clear users data in store
+      this.$store.dispatch('clearUsers')
+    }
   }
 }
 </script>
